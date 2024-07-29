923d981b2eddc23231f9241dd0c49c8c116ec613
aerugo <hugiasgeirsson@gmail.com>
2024-07-29 17:28:25 +0200

    Add global setup and teardown to run test API and reset test database

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,4 +1,8 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
+import { exec } from 'child_process';
+import { promisify } from 'util';
+
+const execAsync = promisify(exec);
 
 const config: PlaywrightTestConfig = {
 	webServer: {
@@ -6,7 +10,24 @@ const config: PlaywrightTestConfig = {
 		port: 4173
 	},
 	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+	globalSetup: async () => {
+		// Start the test API
+		exec('TEST_MODE=1 uvicorn backend.kanot.main:app --host localhost --port 8888 &');
+		
+		// Wait for the API to start
+		await new Promise(resolve => setTimeout(resolve, 5000));
+		
+		// Reset the test database
+		await execAsync('python ./scripts/populate_test_db.py');
+	},
+	globalTeardown: async () => {
+		// Stop the test API
+		await execAsync('pkill -f "uvicorn backend.kanot.main:app"');
+	},
+	use: {
+		baseURL: 'http://localhost:4173',
+	},
 };
 
 export default config;
